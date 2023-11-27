81cb8ee7a8dec89285ccecac57b1795062748dd9
Success Imamun <SuccessImamun@gmail.com>
2023-11-27 06:03:33 +0100

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,10 @@ import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { CategoryListComponent } from './features/category/category-list/category-list.component';
 import { AddCategoryComponent } from './features/category/add-category/add-category.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { EditCategoryComponent } from './features/category/edit-category/edit-category.component';
+import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpost-list.component';
+import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blogpost.component'
 
 @NgModule({
   declarations: [
@@ -15,6 +18,9 @@ import { HttpClientModule } from '@angular/common/http'
     NavbarComponent,
     CategoryListComponent,
     AddCategoryComponent,
+    EditCategoryComponent,
+    BlogpostListComponent,
+    AddBlogpostComponent,
   ],
   imports: [
     BrowserModule,
